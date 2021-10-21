7f230bb2674f38ab691be9d30e4b6499c34f4c8b
jitu712 <jitu712@gmail.com>
2021-10-21 18:02:10 +0530

    add iam role to allow putitem

--- src/handlers/auction.js
+++ src/handlers/auction.js
@@ -1,12 +1,23 @@
+import { v4 as uuid } from "uuid";
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 const createAuction = async (event, context) => {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString()
   };
+
+  await dynamodb.put({
+    TableName: 'AuctionTable',
+    Item: auction
+  }).promise();
 
   return {
     statusCode: 201,
