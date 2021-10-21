403c7a65880a41938130decce6cc3b856121a664
jitu712 <jitu712@gmail.com>
2021-10-21 17:22:26 +0530

    first commit

---
+++ src/handlers/auction.js
@@ -0,0 +1,17 @@
+const createAuction = async (event, context) => {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString()
+  };
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction)
+  };
+};
+
+export const handler = createAuction;
